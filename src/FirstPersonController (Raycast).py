b85e77cdcbefa4acc9d7e446837dc6b964f3c1f9
y2bad <75477334+y2bad@users.noreply.github.com>
2023-08-27 00:45:51 +0200

    Add files via upload

---
+++ src/FirstPersonController (Raycast).py
@@ -0,0 +1,146 @@
+from ursina import *
+
+
+class FirstPersonController(Entity):
+    
+    def __init__(self, **kwargs):
+        self.cursor = Entity(parent=camera.ui, model='quad', color=color.pink, scale=.008, rotation_z=45)
+        super().__init__()
+        self.speed = 6
+        self.acceleration = 12
+        self.height = 2
+
+        self.camera_pivot = Entity(parent=self, y=self.height)
+        camera.parent = self.camera_pivot
+        camera.position = Vec3(0,0,0)
+        camera.rotation = Vec3(0,0,0)
+        camera.fov = 90
+        mouse.locked = True
+        self.mouse_sensitivity = Vec2(60,60)
+
+        self.gravity = 1
+        self.jumping = False
+        self.grounded = False
+        self.jump_height = 2
+        self.jump_up_duration = .5
+        self.fall_after = .35 # will interrupt jump up
+        self.air_time = 0
+        self.direction = Vec3(0,0,0)
+        self.velocity = Vec3(0,0,0)
+
+        for key, value in kwargs.items():
+            setattr(self, key ,value)
+
+        # make sure we don't fall through the ground if we start inside it
+        if self.gravity:
+            ray = raycast(self.world_position+Vec3(0,self.height,0), self.down, ignore=(self,))
+            if ray.hit:
+                self.y = ray.world_point.y
+
+
+    def update(self):
+        self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
+
+        self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
+        self.camera_pivot.rotation_x = clamp(self.camera_pivot.rotation_x, -90, 90)
+
+        self.direction = Vec3(
+            self.forward * (held_keys['w'] - held_keys['s'])
+            + self.right * (held_keys['d'] - held_keys['a'])
+            ).normalized()
+
+        self.velocity = lerp(self.velocity, self.direction * self.speed, self.acceleration * time.dt)
+        self.distance = self.velocity.length() * time.dt
+
+        # make sure we don't walk through walls
+        for i in range(2):
+            feet_ray = raycast(self.position+Vec3(0,0.5,0), self.velocity, ignore=(self,))
+            head_ray = raycast(self.position+Vec3(0,self.height-.1,0), self.velocity, ignore=(self,))
+
+            if feet_ray.hit:
+                collision_normal = Vec3(feet_ray.world_normal.x, 0, feet_ray.world_normal.z).normalized()
+                collision_distance = feet_ray.distance * collision_normal.dot(self.velocity.normalized())
+                if -(self.distance+.5) < collision_distance < self.distance+.5:
+                    self.velocity -= collision_normal * collision_normal.dot(self.velocity)
+                    if i > 0: self.velocity = Vec3(0,0,0)
+
+            elif head_ray.hit:
+                collision_normal = Vec3(head_ray.world_normal.x, 0, head_ray.world_normal.z).normalized()
+                collision_distance = head_ray.distance * collision_normal.dot(self.velocity.normalized())
+                if -(self.distance+.5) < collision_distance < self.distance+.5:
+                    self.velocity -= collision_normal * collision_normal.dot(self.velocity)
+                    if i > 0: self.velocity = Vec3(0,0,0)
+
+        self.position += self.velocity * time.dt
+
+        # gravity
+        if self.gravity:
+            ray = raycast(self.world_position+Vec3(0,self.height,0), self.down, ignore=(self,))
+
+            if ray.distance <= self.height+.1:
+                self.air_time = 0
+                self.grounded = True
+                # make sure it's not a wall and that the point is not too far up
+                if ray.world_normal.y > .7 and ray.world_point.y - self.world_y < .5: # walk up slope
+                    self.y = ray.world_point[1]
+            else:
+                self.grounded = False
+
+                # if not on ground and not on way up in jump, fall
+                self.y -= min(self.air_time, ray.distance-.05) * time.dt * 100
+                self.air_time += time.dt * .25 * self.gravity
+
+
+    def input(self, key):
+        if key == 'space':
+            self.jump()
+
+
+    def jump(self):
+        if not self.grounded:
+            return
+        
+        self.jumping = True
+        self.grounded = False
+
+        ray = raycast(self.world_position+Vec3(0,self.height-.1,0), self.up, ignore=(self,), distance=.2+self.jump_height)
+        self.animate_y(self.y+min(self.jump_height, ray.distance-.2), self.jump_up_duration, resolution=int(1//time.dt), curve=curve.out_expo)
+        invoke(self.start_fall, delay=self.fall_after)
+
+    
+    def start_fall(self):
+        self.jumping = False
+        self.y_animator.pause()
+
+
+    def on_enable(self):
+        mouse.locked = True
+        self.cursor.enabled = True
+
+
+    def on_disable(self):
+        mouse.locked = False
+        self.cursor.enabled = False
+
+
+if __name__ == "__main__":
+    # from ursina.prefabs.first_person_controller import FirstPersonController
+    app = Ursina(borderless = False)
+
+    ground = Entity(model="plane", texture="grass", collider="box", scale=Vec3(1000, 1, 1000), texture_scale=Vec2(1000, 1000))
+
+    wall1 = Entity(model="cube", texture="brick", collider="box", scale=Vec3(1, 3, 5), position=Vec3(2, 1.5, 0), texture_scale=Vec2(5, 3))
+
+    wall2 = Entity(model="cube", texture="brick", collider="box", scale=Vec3(16, 3, 1), position=Vec3(-5, 1.5, 5), rotation=Vec3(0, 30, 0), texture_scale=Vec2(15, 3))
+
+    ceiling = Entity(model="cube", texture="brick", collider="box", scale=Vec3(3, 1, 5), position=Vec3(3, 3.5, 0), texture_scale=Vec2(5, 3))
+
+    slope = Entity(model="cube", texture="brick", collider="box", scale=Vec3(5, 3, 3), position=Vec3(-5, 1, 0), rotation=Vec3(0, 0, 10), texture_scale=Vec2(5, 3))
+
+    player = FirstPersonController(y=.5)
+
+    def input(key):
+        if key == "escape":
+            mouse.locked = not mouse.locked
+
+    app.run()
---
+++ src/FirstPersonController (Swept AABB).py
@@ -0,0 +1,230 @@
+from ursina import *
+
+
+class AABB:
+    def __init__(self, position: Vec3, shape: list):
+        self.position = position
+        self.shape = shape
+
+
+    @property
+    def x(self):
+        return self.position.x
+
+    @property
+    def y(self):
+        return self.position.y
+
+    @property
+    def z(self):
+        return self.position.z
+
+    @property
+    def x_1(self):
+        return self.shape[0] + self.position.x
+
+    @property
+    def y_1(self):
+        return self.shape[1] + self.position.y
+
+    @property
+    def z_1(self):
+        return self.shape[2] + self.position.z
+
+    @property
+    def x_2(self):
+        return self.shape[3] + self.position.x
+    
+    @property
+    def y_2(self):
+        return self.shape[4] + self.position.y
+    
+    @property
+    def z_2(self):
+        return self.shape[5] + self.position.z
+
+
+class Player(Entity):
+
+    def __init__(self, colliders, **kwargs):
+        super().__init__()
+        self.speed = 6
+        self.acceleration = 16
+        self.gravity = 3.4
+        self.jump_height = 1.4
+
+        self.noclip_speed = 8
+        self.noclip_acceleration = 6
+
+        self.colliders = colliders
+        self.aabb_collider = AABB(self.position, [-.5, -1.5, -.5,  .5, .4, .5])
+
+        self.noclip_mode = False
+
+        self.camera_pivot = Entity(parent=self)
+        camera.parent = self.camera_pivot
+        camera.position = Vec3(0,0,0)
+        camera.rotation = Vec3(0,0,0)
+        camera.fov = 90
+        self.mouse_sensitivity = 80
+
+        self.grounded = False
+        self.airtime = 0
+        self.direction = Vec3(0,0,0)
+        self.velocity = Vec3(0,0,0)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    
+    def aabb_broadphase(self, collider_1, collider_2, direction):
+        x_1 = min(collider_1.x_1, collider_1.x_1 + direction.x)
+        y_1 = min(collider_1.y_1, collider_1.y_1 + direction.y)
+        z_1 = min(collider_1.z_1, collider_1.z_1 + direction.z)
+
+        x_2 = max(collider_1.x_2, collider_1.x_2 + direction.x)
+        y_2 = max(collider_1.y_2, collider_1.y_2 + direction.y)
+        z_2 = max(collider_1.z_2, collider_1.z_2 + direction.z)
+
+        return not (x_2 < collider_2.x_1 or
+                    x_1 > collider_2.x_2 or
+                    y_2 < collider_2.y_1 or
+                    y_1 > collider_2.y_2 or
+                    z_2 < collider_2.z_1 or
+                    z_1 > collider_2.z_2)
+
+
+    def swept_aabb(self, collider_1, collider_2, direction):
+        x_inv_entry = collider_2.x_1 - collider_1.x_2 if direction.x > 0 else collider_2.x_2 - collider_1.x_1
+        x_inv_exit = collider_2.x_2 - collider_1.x_1 if direction.x > 0 else collider_2.x_1 - collider_1.x_2
+
+        y_inv_entry = collider_2.y_1 - collider_1.y_2 if direction.y > 0 else collider_2.y_2 - collider_1.y_1
+        y_inv_exit = collider_2.y_2 - collider_1.y_1 if direction.y > 0 else collider_2.y_1 - collider_1.y_2
+
+        z_inv_entry = collider_2.z_1 - collider_1.z_2 if direction.z > 0 else collider_2.z_2 - collider_1.z_1
+        z_inv_exit = collider_2.z_2 - collider_1.z_1 if direction.z > 0 else collider_2.z_1 - collider_1.z_2
+
+        x_entry = -inf if direction.x == 0 else x_inv_entry / direction.x
+        x_exit = inf if direction.x == 0 else x_inv_exit / direction.x
+
+        y_entry = -inf if direction.y == 0 else y_inv_entry / direction.y
+        y_exit = inf if direction.y == 0 else y_inv_exit / direction.y
+
+        z_entry = -inf if direction.z == 0 else z_inv_entry / direction.z
+        z_exit = inf if direction.z == 0 else z_inv_exit / direction.z
+
+        entry_time = max([x_entry, y_entry, z_entry])
+        exit_time = min([x_exit, y_exit, z_exit])
+
+        if entry_time > exit_time or x_entry < 0 and y_entry < 0 and z_entry < 0 or x_entry > 1 or y_entry > 1 or z_entry > 1:
+            return 1, Vec3(0, 0, 0)
+
+        normal_x = (0, -1 if direction.x > 0 else 1)[entry_time == x_entry]
+        normal_y = (0, -1 if direction.y > 0 else 1)[entry_time == y_entry]
+        normal_z = (0, -1 if direction.z > 0 else 1)[entry_time == z_entry]
+
+        return entry_time, Vec3(normal_x, normal_y, normal_z)
+
+
+    def update(self):
+        if self.noclip_mode:
+            self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity
+
+            self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity
+            self.camera_pivot.rotation_x = clamp(self.camera_pivot.rotation_x, -90, 90)
+
+            self.direction = Vec3(self.camera_pivot.forward * (held_keys["w"] - held_keys["s"])
+                                  + self.right * (held_keys["d"] - held_keys["a"])).normalized()
+
+            self.direction += self.up * (held_keys["e"] - held_keys["q"])
+
+            self.velocity = lerp(self.velocity, self.direction * self.noclip_speed * time.dt, self.noclip_acceleration * time.dt)
+
+            self.position += self.velocity
+        
+        else:
+            self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity
+
+            self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity
+            self.camera_pivot.rotation_x = clamp(self.camera_pivot.rotation_x, -90, 90)
+
+            self.direction = Vec3(self.forward * (held_keys["w"] - held_keys["s"])
+                                  + self.right * (held_keys["d"] - held_keys["a"])).normalized()
+            
+            self.airtime += time.dt
+
+            self.direction.y -= self.gravity * self.airtime
+
+            self.velocity = lerp(self.velocity, self.direction * self.speed * time.dt, self.acceleration * time.dt)
+
+            self.aabb_collider.position = self.position
+
+            for _ in range(3):
+                collisions = []
+
+                for collider in self.colliders:
+                    if self.aabb_broadphase(self.aabb_collider, collider, self.velocity):
+                        collision_time, collision_normal = self.swept_aabb(self.aabb_collider, collider, self.velocity)
+
+                        collisions.append((collision_time, collision_normal))
+
+                if not collisions:
+                    break
+
+                collision_time, collision_normal = min(collisions, key= lambda x: x[0])
+
+                remaining_time = 1 - collision_time
+
+                response = self.velocity - self.velocity.project(collision_normal)
+                response = Vec3.zero if response.is_nan() else response * remaining_time
+
+                self.velocity = self.velocity * collision_time + response
+
+            if self.velocity.y == 0:
+                self.grounded = True
+                self.airtime = 0
+
+            else:
+                self.grounded = False
+
+            self.position += self.velocity
+
+
+    def input(self, key):
+        if key == "space":
+            if self.grounded:
+                self.velocity.y += self.jump_height / self.gravity
+
+
+    def on_enable(self):
+        mouse.locked = True
+
+
+    def on_disable(self):
+        mouse.locked = False
+
+
+if __name__ == "__main__":
+    app = Ursina(borderless=False)
+
+    colliders = []
+
+    box_1 = Entity(model="cube", texture="brick", position=Vec3(4, 3, 0), scale=Vec3(2, 1, 3), texture_scale=Vec2(2, 3))
+    box_2 = Entity(model="cube", texture="brick", position=Vec3(3, 1.5, 0), scale=Vec3(1, 2, 3), texture_scale=Vec2(2, 3))
+    box_3 = Entity(model="cube", texture="grass", position=Vec3(0, 0, 0), scale=Vec3(25, 1, 25), texture_scale=Vec2(25, 25))
+    box_4 = Entity(model="cube", texture="brick", position=Vec3(-2, 1.5, 0), scale=Vec3(2, 2, 2), texture_scale=Vec2(2, 2))
+
+    collider_1 = AABB(box_1.position, [-1, -.5, -1.5,  1, .5, 1.5])
+    collider_2 = AABB(box_2.position, [-.5, -1, -1.5,  .5, 1, 1.5])
+    collider_3 = AABB(box_3.position, [-12.5, -.5, -12.5,  12.5, .5, 12.5])
+    collider_4 = AABB(box_4.position, [-1, -1, -1,  1, 1, 1])
+
+    colliders.extend([collider_1, collider_2, collider_3, collider_4])
+
+    player = Player(colliders, position=Vec3(0, 5, 0))
+
+    def input(key):
+        if key == "escape":
+            mouse.locked = not mouse.locked
+
+    app.run()
